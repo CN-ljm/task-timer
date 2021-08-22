b5031a5c0fc71cec65ee0560633091a08d601d9a
liangjiaming <liangjiaming@linklogis.com>
2021-08-22 15:06:12 +0800

    初始化工程

--- src/main/java/com/learn/App.java
+++ src/main/java/com/learn/App.java
@@ -1,9 +1,18 @@
 package com.learn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 /**
  * @author Created by liangjiaming on 2021/8/22
  * @title
  * @Desc
  */
+@SpringBootApplication
 public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+        System.out.println("hello world");
+    }
 }
